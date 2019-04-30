daf41e0f3a57b5ad85f45b0398dc67c86d7f7399
Deep XPS <dghaghara@gmail.com>
2019-04-29 20:07:19 -0400

    progress on view rides

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 <ul>
 	<li><a href="book_ride.php"><strong>Book A Ride!</strong></a></li>
-	<li><strong>View Ride</strong></a> - View a ride. Work in progress.</li>
+	<li><a href="view_ride.php"><strong>View Ride</strong></a></li>
 	<li><strong>Modify Ride</strong></a> - edit previusly booked rides. Work in progress.</li>
 	<li><strong>Delete Ride</strong></a> - Cancel a ride. Work in progress.</li>
 </ul>
